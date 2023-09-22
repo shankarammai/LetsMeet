8c3578d4cbb55d980072ed2ba3540b8e52a6a90b
Shankar Ammai <ammai.shankar@gmail.com>
2023-09-22 23:41:57 +0100

    * Remove Unused Vars * Remove Debug Code * Refactor

--- src/Room.tsx
+++ src/Room.tsx
@@ -1,22 +1,18 @@
-import { useEffect, useMemo, useRef, useState } from 'react';
-import Peer, { DataConnection, MediaConnection } from 'peerjs';
+import { useEffect, useRef, useState } from 'react';
+import Peer, { MediaConnection } from 'peerjs';
 import { Affix, Button, Container, Drawer, Group, Input, Text, rem, Flex, ActionIcon, TextInput, CopyButton, } from '@mantine/core';
 import VideoPlayer from './store/VideoPlayer';
 import { useDisclosure } from '@mantine/hooks';
 import SideBar from './SideBar';
 import { useLocation, useNavigate } from 'react-router-dom';
-import getUuidByString from 'uuid-by-string';
 import { useAtom } from 'jotai';
-import { messagesAtom, remoteDataConnectionAtom, peerIdAtom, videoLayersAtom, connectionUserNamesAtom, mediaConnectionsAtom } from './store/store';
+import { messagesAtom, remoteDataConnectionAtom, peerIdAtom, connectionUserNamesAtom, mediaConnectionsAtom } from './store/store';
 import { Message } from './Types';
 import { BsMicMuteFill, BsFillCameraVideoOffFill, BsCameraVideoFill, BsFillClipboardFill, BsFillClipboardCheckFill } from 'react-icons/bs';
 import { FaMicrophone, FaUserAlt } from 'react-icons/fa';
 import { AiOutlineFundProjectionScreen } from 'react-icons/ai';
 import { BiPhoneCall } from 'react-icons/bi';
 import { notifications } from '@mantine/notifications';
-import etro from 'etro';
-
-
 
 function App() {
     const navigate = useNavigate();
@@ -25,13 +21,9 @@ function App() {
         navigate('/');
     }
     const userName = state.userName;
-    const userUniqueID = state.userUniqueID;
-    const myPeerID = useMemo(() => getUuidByString(userUniqueID), [userUniqueID])
 
     const [peerId, setPeerId] = useAtom(peerIdAtom);
     const [remotePeerIdValue, setRemotePeerIdValue] = useState('');
-    const remoteVideoRef = useRef<HTMLVideoElement>(null);
-    const currentUserVideoRef = useRef<HTMLVideoElement>(null);
     const [myVideoStream, setMyVideoStream] = useState<MediaStream>(new MediaStream());
     const peerInstance = useRef<Peer | null>(null);
     const [remoteMediaStreams, setRemoteMediaStreams] = useState<MediaStream[]>([]);
@@ -40,10 +32,9 @@ function App() {
     const [connectionUserNames, setConnectionUserNames] = useAtom(connectionUserNamesAtom);
     const [opened, { open, close }] = useDisclosure(false);
     const [messages, setMessages] = useAtom(messagesAtom);
-    const [isMuted, setIsMuted] = useState<boolean>(true);
+    const [isAudioOn, setIsAudioOn] = useState<boolean>(true);
     const [isVideoOn, setIsVideoOn] = useState<boolean>(true);
-    const [isShareScreen, setIsShareScreen] = useState<boolean>(true);
-    const [videoLayers, setVideoLayers] = useAtom(videoLayersAtom);
+    const [isShareScreen, setIsShareScreen] = useState<boolean>(false);
     const canvasRef = useRef(null);
 
     useEffect(() => {
@@ -57,7 +48,6 @@ function App() {
         });
 
         peer.on('close', () => {
-            console.log('Data ended from another');
             closeCall();
         });
 
@@ -66,14 +56,11 @@ function App() {
             console.log('connecton established');
             setRemoteDataConnections((previous) => [connection, ...previous]);
             connection.on('data', (data) => {
-                console.log('data received');
-                console.log(data);
                 setMessages((prev) => [...prev, data as Message]);
             })
         });
 
         peer.on('call', (call) => {
-            console.log('call event tigerred');
             const metaData = call.metadata;
             notifications.show({
                 id: 'incomingCallNotification',
@@ -100,7 +87,6 @@ function App() {
 
             //When the connection is closed
             call.on('close', () => {
-                console.log('Call Ended from remote person');
                 closeCall();
             })
         });
@@ -135,7 +121,6 @@ function App() {
             const con = peerInstance.current!.connect(remotePeerId, options);
 
             con.on('open', () => {
-                console.log('Connection opened on caller');
                 setRemoteDataConnections(prev => [con, ...prev]);
             });
             con.on('close', () => {
@@ -144,8 +129,6 @@ function App() {
             });
 
             con.on('data', function (data) {
-                console.log('Connection data received line 151');
-                console.log(data);
                 if ((data as Message).dataType == 'InitialConfig') {
                     console.log(data);
                     setConnectionUserNames([(data as Message).data]);
@@ -171,7 +154,6 @@ function App() {
         navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then((mediaStream) => {
             call.answer(mediaStream);
             call.on('stream', (remoteStream) => {
-                console.log('stream tiggered line 32');
                 setRemoteMediaStreams([remoteStream]);
             });
         });
@@ -181,7 +163,7 @@ function App() {
         const canvas = canvasRef.current;
         const ctx = canvas.getContext('2d');
         // Define the background color and text color
-        const backgroundColor = '#3498db'; // Background color (blue)
+        const backgroundColor = '#000000'; // Background color (blue)
         const textColor = '#FFFFFF'; // Text color (white)
         ctx.fillStyle = backgroundColor;
         ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -199,8 +181,6 @@ function App() {
     };
 
     const closeCall = () => {
-        //Todo: disconnect, messages resst, connections,video reset
-        console.log('Call Ended trigerred');
         remoteDataConnections[0]?.close();
         mediaConnections[0]?.close();
         setMessages([]);
@@ -226,27 +206,23 @@ function App() {
     }
 
     const handleVoiceToggle = () => {
-        setIsMuted(!isMuted);
+        setIsAudioOn(!isAudioOn);
         if (mediaConnections.length > 0) {
-            mediaConnections[0].localStream.getAudioTracks()[0].enabled = isMuted ? false : true;
+            mediaConnections[0].localStream.getAudioTracks()[0].enabled = isAudioOn ? false : true;
         }
     }
 
     const handleVideoToggle = async () => {
         setIsVideoOn(!isVideoOn);
-
         myVideoStream.getVideoTracks()[0].enabled = !isVideoOn;
-
-        if (isVideoOn) {
-            const whiteVideoTrack = showVideoOffScreen().getVideoTracks()[0];
-            const videoSender = mediaConnections[0]?.peerConnection.getSenders().find(sender => sender?.track?.kind === 'video');
-            videoSender?.replaceTrack(whiteVideoTrack);
-            setMyVideoStream(showVideoOffScreen);
-        } else {
-            const videoSender = mediaConnections[0]?.peerConnection.getSenders().find(sender => sender?.track?.kind === 'video');
-            const webcam = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
-            videoSender?.replaceTrack(webcam.getVideoTracks()[0]);
-            setMyVideoStream(webcam);
+        const videoSender = mediaConnections[0]?.peerConnection.getSenders().find(sender => sender?.track?.kind === 'video');
+        if (videoSender) {
+            const videoStream = isVideoOn
+                ? showVideoOffScreen()
+                : await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+            const videoTrack = videoStream.getVideoTracks()[0];
+            videoSender?.replaceTrack(videoTrack);
+            setMyVideoStream(videoStream);
         }
     };
 
@@ -255,9 +231,7 @@ function App() {
             const stream = isShareScreen
                 ? await navigator.mediaDevices.getDisplayMedia({ video: true, audio: true })
                 : await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
-
-            const videoSender = mediaConnections[0].peerConnection.getSenders().find(sender => sender!.track.kind === 'video');
-
+            const videoSender = mediaConnections[0].peerConnection.getSenders().find(sender => sender?.track?.kind === 'video');
             if (videoSender) {
                 videoSender.replaceTrack(stream.getVideoTracks()[0]);
             }
@@ -320,10 +294,9 @@ function App() {
             }
             <canvas ref={canvasRef} style={{ display: 'none' }} />
             <Group position='center' mt={'lg'}>
-                <Button color="indigo" onClick={handleVoiceToggle}>{isMuted ? (<FaMicrophone />) : (<BsMicMuteFill />)}</Button>
+                <Button color="indigo" onClick={handleVoiceToggle}>{isAudioOn ? (<FaMicrophone />) : (<BsMicMuteFill />)}</Button>
                 <Button color="grape" onClick={handleVideoToggle}>{isVideoOn ? <BsCameraVideoFill /> : <BsFillCameraVideoOffFill />}</Button>
                 <Button color="lime" onClick={handleVideoSourceToggle}> {isShareScreen ? <FaUserAlt /> : <AiOutlineFundProjectionScreen />}</Button>
-                <Button color="lime" > Record Locally</Button>
                 <Button color="red" onClick={closeCall} > End</Button>
             </Group>
         </Container>
